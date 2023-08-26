d8954471bdfa4785f1e2d6446f3312d2d6166b1b
Miguel Guzmán <40463442+miguelguzmanr@users.noreply.github.com>
2023-08-26 00:21:51 -0400

    Add tabler-icons

---
+++ lib/tabler_icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class TablerIcons {
+  static const String fontFamily = 'tabler-icons';
+  static const String? fontPackage = null;
+
+  static const IconData home =
+      IconData(0xeac1, fontFamily: fontFamily, fontPackage: fontPackage);
+  static const IconData smartHome =
+      IconData(0xecde, fontFamily: fontFamily, fontPackage: fontPackage);
+  static const IconData search =
+      IconData(0xeb1c, fontFamily: fontFamily, fontPackage: fontPackage);
+  static const IconData ticket =
+      IconData(0xeb3d, fontFamily: fontFamily, fontPackage: fontPackage);
+  static const IconData notifications =
+      IconData(0xea35, fontFamily: fontFamily, fontPackage: fontPackage);
+}
